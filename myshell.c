8514d382c1b45d984be81d3a0bb3d0184a76a4f3
talsabel1 <120895203+talsabel1@users.noreply.github.com>
2023-04-29 22:22:53 +0300

    finished first draft, before debug

--- myshell.c
+++ myshell.c
@@ -5,49 +5,45 @@
 #include <errno.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <signal.h>
 
 #define PIPE "|"
-#define RUN_IN_BG "&"
+#define RUN_IN_BACKGROUND "&"
 #define INPUT_REDIRECT "<"
 
 
 int process_arglist(int count, char** arglist);
-int myPipe(int count, char **arglist, int index);
+int regular_execution(int count, char **arglist);
+int my_pipe(int count, char **arglist, int pipe_index);
 int background(int count, char **arglist);
 int redirect(int count, char **arglist);
 int prepare(void);
 int finalize(void);
 void error(const char *type);
-
+void signal_handler(int signal_number);
+void default_signal();
 
 int process_arglist(int count, char **arglist) {
     int i;
-    int pipe;
-    int bg;
-    int inputRedirect;
     char* curr;
-    pid_t pid;
 
     for (i = 0; i < count; i++) {
         curr = arglist[i];
 
         if (strcmp(curr, PIPE) == 0) {
-            pipe = myPipe(count, arglist, i);
-            if (pipe == -1) {
+            if (my_pipe(count, arglist, i) == -1) {
                 return 0;
             }
             break;
         }
         else if (strcmp(curr, INPUT_REDIRECT) == 0) {
-            inputRedirect = redirect(count, arglist);
-            if (inputRedirect == -1) {
+            if (redirect(count, arglist) == -1) {
                 return 0;
             }
             break;
         }
-        else if (strcmp(curr, RUN_IN_BG) == 0) {
-            bg = background(count, arglist);
-            if (bg == -1) {
+        else if (strcmp(curr, RUN_IN_BACKGROUND) == 0) {
+            if (background(count, arglist) == -1) {
                 return 0;
             }
             break;
@@ -55,67 +51,77 @@ int process_arglist(int count, char **arglist) {
     }
 
     if (i == count) { //the for loop finished (the command didn't contain a shell symbol)
-        pid = fork();
-        if (pid == 0) {   // child process
-            if (execvp(arglist[0], arglist) == -1){
-                error("execvp");
-                exit(1);
-            }
-        } else if (pid > 0) {   // parent process
-            if((wait(NULL) == -1) && !(errno == ECHILD || errno == EINTR)) {
-                error("wait");
-                return 0;
-            }
-        } else {    // fork failed
-            error("fork");
+        if (regular_execution(count, arglist) == -1) {
             return 0;
         }
-
     }
 
     return 1;
 }
 
+int regular_execution(int count, char **arglist) {
+    pid_t pid;
 
-int myPipe(int count, char **arglist, int pipeIndex){
+    pid = fork();
+    if (pid == 0) {   // child process
+        default_signal();
+        if (execvp(arglist[0], arglist) == -1){
+            error("execvp");
+            exit(1);
+        }
+    } else if (pid > 0) {   // parent process
+        if((wait(NULL) == -1) && !(errno == ECHILD || errno == EINTR)) { // coverage for EINTR is handled by SA_RESTART in sigaction so this check is just an extra precaution
+            error("wait");
+            return -1;
+        }
+    } else {    // fork failed
+        error("fork");
+        return -1;
+    }
+    return 1;
+}
+
+int my_pipe(int count, char **arglist, int pipe_index){
     int pipefd[2];
-    pid_t pid1;
-    pid_t pid2;
+    pid_t pid_1;
+    pid_t pid_2;
 
     if (-1 == pipe(pipefd)) {
         error("pipe");
         return -1;
     }
     else {
-        pid1 = fork();
-        if (pid1 == 0) {    // 1st child process (to write to pipe)
+        pid_1 = fork();
+        if (pid_1 == 0) {    // 1st child process (to write to pipe)
+            default_signal();
             close(pipefd[0]);
             if (dup2(pipefd[1], STDOUT_FILENO) == -1) {
                 error("dup2");
                 exit(1);
             }
-            arglist[pipeIndex] = NULL;
+            close(pipefd[1]);
+            arglist[pipe_index] = NULL;
             if(execvp(arglist[0], arglist) == -1){
                 error("execvp");
                 exit(1);
             }
         }
-        else if (pid1 > 0) {      // parent process
-            //wait();
-            // wait error?
-            pid2 = fork();
-            if (pid2 == 0) { // 2nd child process (to read from pipe)
+        else if (pid_1 > 0) {      // parent process
+            pid_2 = fork(); // maybe take this outside of else if so it runs at same time as first fork()
+            if (pid_2 == 0) { // 2nd child process (to read from pipe)
+                default_signal();
                 close(pipefd[1]);
                 if(dup2(pipefd[0], STDIN_FILENO) == -1){
                     error("dup2");
                     exit(1);
                 }
-                if (execvp(arglist[pipeIndex+1], &arglist[pipeIndex+1]) == -1){
+                close(pipefd[0]);
+                if (execvp(arglist[pipe_index + 1], &arglist[pipe_index + 1]) == -1){
                     error("execvp");
                     exit(1);
                 }
             }
-            else if (pid2 > 0) {
+            else if (pid_2 > 0) {
                 close(pipefd[0]);
                 close(pipefd[1]);
                 if((wait(NULL) == -1) && !(errno == ECHILD || errno == EINTR)) {
@@ -155,6 +161,7 @@ int redirect(int count, char **arglist){
     else {
         pid = fork();
         if (pid == 0) {    // child process
+            default_signal();
             if (dup2(fd, STDOUT_FILENO) == -1) {
                 error("dup2");
                 exit(1);
@@ -197,31 +204,78 @@ int background(int count, char **arglist) {
 }
 
 int prepare(void) {
-    // treat "no termination of shell on SIGINT"
-    // foreground (child) processes should terminate on SIGINT
-    // background (child) processes should not terminate on SIGINT
+//    signal_handler(SIGCHLD);
+    signal_handler(SIGINT);
+
+    // handle SIG_CHLD
+
 }
 
 
 int finalize(void) {}
 
+void signal_handler(int signal_number) {
+    /*
+     * I used this link as a reference to assist in understanding how to write the code for signal handling:
+     * https://www.gnu.org/software/libc/manual/html_node/Sigaction-Function-Example.html
+     */
+
+    struct sigaction new_action;
+
+    if (signal_number == SIGINT) {
+        new_action.sa_handler = SIG_IGN;
+        sigemptyset (&new_action.sa_mask);
+        new_action.sa_flags = SA_RESTART;
+        if (sigaction(SIGINT, &new_action, NULL) == -1) {
+            error("signal");
+            exit(1);
+        }
+    }
+
+//    else if (signal_number == SIGCHLD) {
+//        // add fields for new_action
+//        new_action.sa_handler;
+//        sigemptyset (&new_action.sa_mask);
+//        new_action.sa_flags = SA_RESTART | SA_NOCLDSTOP;
+//        if (sigaction(SIGCHLD, &new_action, NULL) == -1) {
+//            error("signal");
+//            exit(1);
+//        }
+//    }
+}
+
+void default_signal() {
+    struct sigaction new_action;
+
+    new_action.sa_handler = SIG_DFL;
+    sigemptyset (&new_action.sa_mask);
+    new_action.sa_flags = SA_RESTART;
+    if (sigaction(SIGINT, &new_action, NULL) == -1) {
+        error("signal");
+        exit(1);
+    }
+}
+
 void error(const char *type) {
     if (strcmp(type, "pipe") == 0){
-        perror("Pipe creation failed.");
+        perror("Error: Pipe creation failed.");
     }
     else if (strcmp(type, "dup2") == 0){
-        perror("Call to dup2() failed.");
+        perror("Error: Call to dup2() failed.");
     }
     else if (strcmp(type, "execvp") == 0) {
-        perror("Call to execvp() failed.");
+        perror("Error: Call to execvp() failed.");
     }
     else if (strcmp(type, "fork") == 0) {
-        perror("Fork creation failed.");
+        perror("Error: Fork creation failed.");
     }
     else if (strcmp(type, "file") == 0) {
-        perror("Error opening file.");
+        perror("Error: Call to open() failed. Couldn't open file.");
     }
     else if (strcmp(type, "wait") == 0) {
-        perror("Call to wait() failed.");
+        perror("Error: Call to wait() failed.");
+    }
+    else if (strcmp(type, "signal") == 0) {
+        perror("Error: Call to sigaction() failed.");
     }
 }
