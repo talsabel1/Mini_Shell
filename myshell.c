2708e8ad456a28819aae39033eb36a6c769a17b1
talsabel1 <120895203+talsabel1@users.noreply.github.com>
2023-04-30 21:25:37 +0300

    final

--- myshell.c
+++ myshell.c
@@ -12,19 +12,48 @@
 #define RUN_IN_BACKGROUND "&"
 #define INPUT_REDIRECT "<"
 
-
+/* function declarations ----------------------------------------------------------------------------------------------*/
+int prepare(void);
 int process_arglist(int count, char** arglist);
-int regular_execution(int count, char **arglist);
-int my_pipe(int count, char **arglist, int pipe_index);
+int regular_execution(char **arglist);
+int my_pipe(char **arglist, int pipe_index);
 int background(int count, char **arglist);
 int redirect(int count, char **arglist);
-int prepare(void);
 int finalize(void);
 void error(const char *type);
-void signal_handler(int signal_number);
-void restore_default_signal();
-//void sigchld_handler(int signal);
+void restore_default_signals();
+/*---------------------------------------------------------------------------------------------------------------------*/
 
+/* Set up function, defines SIGINT and SIGCHLD responses --------------------------------------------------------------*/
+int prepare(void) {
+    /*
+     * I used this link as a reference to assist in understanding how to write the code for signal handling:
+     * https://www.gnu.org/software/libc/manual/html_node/Sigaction-Function-Example.html
+     */
+
+    struct sigaction new_action_sigint, new_action_sigchld;
+
+    new_action_sigint.sa_handler = SIG_IGN;
+    sigemptyset (&new_action_sigint.sa_mask);
+    new_action_sigint.sa_flags = SA_RESTART;
+    if (sigaction(SIGINT, &new_action_sigint, NULL) == -1) {
+        error("signal");
+        exit(1);
+    }
+
+    new_action_sigchld.sa_handler = SIG_IGN;
+    sigemptyset (&new_action_sigchld.sa_mask);
+    new_action_sigint.sa_flags = SA_RESTART | SA_NOCLDSTOP;
+    if (sigaction(SIGCHLD, &new_action_sigchld, NULL) == -1) {
+        error("signal");
+        exit(1);
+    }
+
+    return 0;
+}
+
+/* "Main" function that processes commands -------------------------------------------------------------------------------
+ *  Receives array of strings that hold the command + args, and the length of the array. Returns 1 on success, 0 on failure */
 int process_arglist(int count, char **arglist) {
     int i;
     char* curr;
@@ -33,7 +62,7 @@ int process_arglist(int count, char **arglist) {
         curr = arglist[i];
 
         if (strcmp(curr, PIPE) == 0) {
-            if (my_pipe(count, arglist, i) == -1) {
+            if (my_pipe(arglist, i) == -1) {
                 return 0;
             }
             break;
@@ -50,8 +79,8 @@ int process_arglist(int count, char **arglist) {
         }
     }
 
-    if (i == count) { //the for loop finished (the command didn't contain a shell symbol)
-        if (regular_execution(count, arglist) == -1) {
+    if (i == count) {      //the for loop finished (the command didn't contain any shell symbol)
+        if (regular_execution(arglist) == -1) {
             return 0;
         }
     }
@@ -59,18 +88,20 @@ int process_arglist(int count, char **arglist) {
     return 1;
 }
 
-int regular_execution(int count, char **arglist) {
+/* Executing a "regular" shell command --------------------------------------------------------------------------------
+ * Receives array of strings that hold the command + args. Returns 1 on success, -1 on failure ------------------------- */
+int regular_execution(char **arglist) {
     pid_t pid;
 
     pid = fork();
     if (pid == 0) {   // child process
-        restore_default_signal();
+        restore_default_signals();
         if (execvp(arglist[0], arglist) == -1){
             error("execvp");
             exit(1);
         }
     } else if (pid > 0) {   // parent process
-        if((waitpid(pid, NULL, WUNTRACED) == -1) && !(errno == ECHILD || errno == EINTR)) { // coverage for EINTR is handled by SA_RESTART in sigaction so this check is just an extra precaution
+        if((waitpid(pid, NULL, WUNTRACED) == -1) && !(errno == ECHILD || errno == EINTR)) { // EINTR is handled by SA_RESTART in sigaction so this check is just an extra precaution
             error("wait");
             return -1;
         }
@@ -81,19 +112,21 @@ int regular_execution(int count, char **arglist) {
     return 1;
 }
 
-int my_pipe(int count, char **arglist, int pipe_index){
+/* Executing a PIPE shell command -------------------------------------------------------------------------------------
+ * Receives array of strings that hold the command + args, and the index of the '|' char. Returns 1 on success, -1 on failure */
+int my_pipe(char **arglist, int pipe_index){
     int pipefd[2];
     pid_t pid_1;
     pid_t pid_2;
 
-    if (-1 == pipe(pipefd)) {
+    if (pipe(pipefd) == -1) {
         error("pipe");
         return -1;
     }
     else {
         pid_1 = fork();
-        if (pid_1 == 0) {    // 1st child process (to write to pipe)
-            restore_default_signal();
+        if (pid_1 == 0) {      // 1st child process (to write to pipe)
+            restore_default_signals();
             close(pipefd[0]);
             if (dup2(pipefd[1], STDOUT_FILENO) == -1) {
                 error("dup2");
@@ -106,10 +139,10 @@ int my_pipe(int count, char **arglist, int pipe_index){
                 exit(1);
             }
         }
-        else if (pid_1 > 0) {      // parent process
-            pid_2 = fork(); // maybe take this outside of else if so it runs at same time as first fork()
-            if (pid_2 == 0) { // 2nd child process (to read from pipe)
-                restore_default_signal();
+        else if (pid_1 > 0) {     // parent process
+            pid_2 = fork();
+            if (pid_2 == 0) {     // 2nd child process (to read from pipe)
+                restore_default_signals();
                 close(pipefd[1]);
                 if(dup2(pipefd[0], STDIN_FILENO) == -1){
                     error("dup2");
@@ -121,19 +154,19 @@ int my_pipe(int count, char **arglist, int pipe_index){
                     exit(1);
                 }
             }
-            else if (pid_2 > 0) {
+            else if (pid_2 > 0) {      // parent process
                 close(pipefd[0]);
                 close(pipefd[1]);
-                if((waitpid(pid_1, NULL, WUNTRACED) == -1) && !(errno == ECHILD || errno == EINTR)) {
+                if((waitpid(pid_1, NULL, WUNTRACED) == -1) && !(errno == ECHILD || errno == EINTR)) { // EINTR is handled by SA_RESTART in sigaction so this check is just an extra precaution
                     error("wait");
                     return -1;
                 }
-                if((waitpid(pid_2, NULL, WUNTRACED) == -1) && !(errno == ECHILD || errno == EINTR)) {
+                if((waitpid(pid_2, NULL, WUNTRACED) == -1) && !(errno == ECHILD || errno == EINTR)) { // EINTR is handled by SA_RESTART in sigaction so this check is just an extra precaution
                     error("wait");
                     return -1;
                 }
             }
-            else { // fork 2 failed
+            else {     // fork 2 failed
                 error("fork");
                 perror("(Second fork)");
                 close(pipefd[0]);
@@ -153,13 +186,15 @@ int my_pipe(int count, char **arglist, int pipe_index){
     return 1;
 }
 
+/* Executing a REDIRECT shell command ---------------------------------------------------------------------------------
+*  Receives array of strings that hold the command + args, and the length of the array. Returns 1 on success, -1 on failure */
 int redirect(int count, char **arglist){
     pid_t pid;
     int fd;
 
     pid = fork();
-    if (pid == 0) {    // child process
-        restore_default_signal();
+    if (pid == 0) {      // child process
+        restore_default_signals();
         if ((fd = open(arglist[count-1], O_RDONLY, 0644)) == -1) {
             error("file");
             exit(1);
@@ -169,17 +204,17 @@ int redirect(int count, char **arglist){
             exit(1);
         }
         close(fd);
-        arglist[count-2] = NULL; // so we can send arglist to execvp w/o < and filename
+        arglist[count-2] = NULL;
         if (execvp(arglist[0], arglist) == -1){
             error("execvp");
             exit(1);
         }
     } else if (pid > 0) {      // parent process
-        if((waitpid(pid, NULL, WUNTRACED) == -1) && !(errno == ECHILD || errno == EINTR)) {
+        if((waitpid(pid, NULL, WUNTRACED) == -1) && !(errno == ECHILD || errno == EINTR)) { // EINTR is handled by SA_RESTART in sigaction so this check is just an extra precaution
             error("wait");
             return -1;
         }
-    } else {     // fork failed
+    } else {      // fork failed
         error("fork");
         return -1;
     }
@@ -187,6 +222,8 @@ int redirect(int count, char **arglist){
     return 1;
 }
 
+/* Executing a BACKGROUND shell command -------------------------------------------------------------------------------
+ * Receives array of strings that hold the command + args, and the length of the array. Returns 1 on success, -1 on failure */
 int background(int count, char **arglist) {
     pid_t pid;
 
@@ -196,69 +233,26 @@ int background(int count, char **arglist) {
             error("signal");
             exit(1);
         }
-        arglist[count-1] = NULL; // remove & to send to execvp
+        arglist[count-1] = NULL;
         if (execvp(arglist[0], arglist) == -1) {
             error("execvp");
             exit(1);
         }
     }
-    else if (pid < 0) { // fork failed
+    else if (pid < 0) {      // fork failed
         error("fork");
         return -1;
     }
     return 1;
-    // parent will just continue, not waiting for child to finish
 }
 
-int prepare(void) {
-    signal_handler(SIGCHLD);
-    signal_handler(SIGINT);
-
-    return 0;
-}
-
-
+/* Clean up function --------------------------------------------------------------------------------------------------*/
 int finalize(void) {
     return 0;
 }
 
-void signal_handler(int signal_number) {
-    /*
-     * I used this link as a reference to assist in understanding how to write the code for signal handling:
-     * https://www.gnu.org/software/libc/manual/html_node/Sigaction-Function-Example.html
-     */
-
-    struct sigaction new_action;
-
-    if (signal_number == SIGINT) {
-        new_action.sa_handler = SIG_IGN;
-        sigemptyset (&new_action.sa_mask);
-        new_action.sa_flags = SA_RESTART;
-        if (sigaction(SIGINT, &new_action, NULL) == -1) {
-            error("signal");
-            exit(1);
-        }
-    }
-
-    else if (signal_number == SIGCHLD) {
-        // add fields for new_action
-        new_action.sa_handler = SIG_IGN;
-        sigemptyset (&new_action.sa_mask);
-        new_action.sa_flags = SA_RESTART | SA_NOCLDSTOP;
-        if (sigaction(SIGCHLD, &new_action, NULL) == -1) {
-            error("signal");
-            exit(1);
-        }
-    }
-}
-
-//void sigchld_handler(int signal) {
-//    int status;
-//
-//    while ((waitpid(-1, &status, WNOHANG)) > 0);
-//}
-
-void restore_default_signal() {
+/* Signal Handling - restores SIGINT and SIGCHLD to default response --------------------------------------------------*/
+void restore_default_signals() {
     struct sigaction new_action_sigint, new_action_sigchld;
 
     new_action_sigint.sa_handler = SIG_DFL;
@@ -278,6 +272,7 @@ void restore_default_signal() {
     }
 }
 
+/* Error handling, receives error type to print the relevant message --------------------------------------------------*/
 void error(const char *type) {
     if (strcmp(type, "pipe") == 0){
         perror("Error: Pipe creation failed.");
